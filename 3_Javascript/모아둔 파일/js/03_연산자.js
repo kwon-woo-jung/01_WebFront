d3665837580c79a476f50152c9e78c7adbf6b88d
kwon-woo-jung <dnwnd1763@gmail.com>
2024-08-30 17:59:22 +0900

    웹프론트 끝

---
+++ 3_Javascript/모아둔 파일/js/03_연산자.js
@@ -0,0 +1,162 @@
+// 변수 선언 및 초기화
+
+// document : HTML 문서 내에서
+// get : 얻다
+// Element : HTML 요소
+// ByID : 아이디가 일치하는
+
+const number1 = document.getElementById("input1");
+const number2 = document.getElementById("input2");
+const result = document.getElementById("calcResult");
+
+console.log(number1);
+
+// 두 수를 더해서 화면에 출력하는 함수
+function plusFn() {
+  // input요소.value : input 요소에 작성된 값 얻어오기
+  const value1 = number1.value;
+  const value2 = number2.value;
+
+  console.log("두 수의 합 :", value1 + value2);
+
+
+// input 요소에 작성된 요소의 값은 무조건 문자열 형태라서
+// 숫자 형태가 아닌 문자열 이어쓰기가 발생해 버린다
+
+// 그래서 해결방법은 문자열(String)을 반드시 숫자(number)로 변경하는 코드를 수행해야 한다
+// 숫자만 작성된 문자열 "123" 을
+// 진짜 숫자타입 123로 바꾸려면
+// Number("123") --> 123 즉 Number를 붙어야 하며 N은 반드시 대문자 여야함
+
+console.log(Number(value1) + Number(value2));
+
+// 두 수의 합을
+// 아이디가 "calcResult"인 요소(result 변수)의
+// 내부 글자(innerText) 대입하기
+result.innerText = (Number(value1) + Number(value2));
+
+}
+
+// 빼기 함수
+function minusFn() {
+const value1 = number1.value;
+const value2 = number2.value;
+
+console.log(value1 - value2);
+console.log(Number(value1) - Number(value2));
+result.innerText = (Number(value1)) - Number(value2);
+
+}
+
+//곱하기 함수
+function multiFn() {
+  const value1 = (number1.value);
+  const value2 = (number2.value);
+
+  result.innerText = value1 * value2;
+}
+
+// 나누기 함수
+
+// moduio 함수
+
+
+// ---------------------------------------------
+// (응용) 입력 받은 3개의 수 합하기
+const num1 = document.getElementById("num1")
+const num2 = document.getElementById("num2")
+const num3 = document.getElementById("num3")
+const total = document.getElementById("total")
+
+function totalFn() {
+  total. innerText = Number(num1.value)
+                   + Number(num2.value)
+                   + Number(num3.value);
+}
+
+// ---------------------------------
+
+// 증가/감소 연산자 (++, --)
+
+// 증가, 감소할 수를 저장할 변수 선언 및 0으로 초기화
+let count = 0;
+// (주의) const로 선언하면 값을 증가/감소 시킬수 없다!
+
+// 필요한 요소 얻어오기
+
+// 문서 내에서 id가 "result2"인 요소를 얻어와 result2 변수에 대입하기
+
+const result2 = document.getElementById("result2");
+
+// 1 증가 함수
+function increase() {
+  // count 변수의 값 1 증가
+  count++; // 또는 ++count; -> count 값이 1 증가
+
+  // 증가한 count 값을 result2의 내부 글자로 대입
+  result2.innerText = count;
+}
+
+
+// 1 감소 함수
+function decrease() {
+  // 감소한 count 값을 result2의 내부 글자로 대입
+  result2.innerText = count--;
+}
+
+// 전위, 후위 연산 확인하기
+function check() {
+  //함수 밖, 안은 구분되는 공간
+  //밖에 작성한 변수 count,
+  //안에 작성한 변수 count
+  // 서로 다른 변수(동명이인)
+  let count = 100;
+
+  // 컴퓨터한테 연산은 +-*/ 뿐만 아니라
+  // 코드를 하나하나 실행하는 것들이 전부 연산
+  // 전위연산은 카운트 값이 먼저 증가 또는 감소후 console에 출력
+
+  // 전위 연산(++count, --count) 확인
+  console.log (++count); // 101
+  console.log (++count); // 102
+  console.log (++count); // 103
+  console.log (--count); // 102
+  console.log (--count); // 101
+  console.log (--count); // 100
+
+  // 후위 연산(count++, count--) 확인
+  // -> 다른 연산이 다 끝나고 마지막에 수행
+
+  count = 50;
+
+  console.log(count++); // 50을 출력한 후 그 뒤에 더해서 51로 증가
+  console.log(count); // 51
+
+  console.log(count--); // 51 출력은 되어서 50이 됬지만 화면상엔 51로 됨
+  console.log(--count); // 50
+
+  let a = 10;
+  let b = 5;
+  let c = ++a * b--;
+
+  // 최종적으로 a,b,c 에 저장된 값은 얼마?
+  // a == 11
+  // b == 4
+  // c == 55
+
+  console.log(a, b, c);
+}
+
+function printJSObject() {
+  const userName = document.getElementById("userName").value;
+  const userAge = document.getElementById("userAge");
+  const userGender = document.getElementById("userGender");
+
+  const userInfo = {
+    "이름" : userName,
+    "나이" : userAge.value,
+    "성별" : userName.value
+  };
+
+  console.log(userInfo);
+}
